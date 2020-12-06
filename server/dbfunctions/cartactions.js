bfc0f8ed293ed566f762d70cde356e1eb66dec90
vaishnav-197 <vaishnavea18@gmail.com>
2020-12-06 19:34:17 +0530

    added delete cart items

--- server/dbfunctions/cartactions.js
+++ server/dbfunctions/cartactions.js
@@ -30,8 +30,67 @@ var cartfunctions = {
              
             
         }
+        else{
+            res.json({success:true , msg:'You are not authorized'})
+        }
 
         
+    },
+
+
+    cartdetails: function(req , res){
+        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+            var token = req.headers.authorization.split(' ')[1]
+            var decodedtoken = jwt.decode(token, config.secret)
+            
+            User.find({email:decodedtoken.email}, function(err , prod){
+                if (err) throw err
+                if(prod){
+                    console.log(prod[0].cart)
+                    return res.json({success: true, prod:  prod[0].cart})
+
+                }
+            })
+   
+            
+        }
+        else{
+            res.json({success:false , msg:'You are not authorized'})
+        }
+    },
+
+
+
+    cartdelete: function(req,res){
+        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+            var token = req.headers.authorization.split(' ')[1]
+            var decodedtoken = jwt.decode(token, config.secret)
+            
+           
+            User.update(
+                {email:decodedtoken.email},
+                {$pull: {cart : { prod: req.body.prod}}},
+                { safe: true, multi:true },
+                (err, obj) => {
+                    if(err) throw err
+                    if(obj){
+
+                        console.log(obj)
+                        res.json({success:'true ' , msg:'product removed'})
+                    }
+
+                })
+            
+          
+            
+             
+            
+        }
+        else
+        {
+            res.json({success:false , msg:'You are not authorized'})
+       
+        }
     }
 
 
